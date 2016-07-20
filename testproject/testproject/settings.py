a3d13b09b1eaa2961c31f68c795d45f9ec51f5c0
Mathias Ertl <mati@er.tl>
2016-07-20 09:19:53 +0200

    add a localsettings.py file

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .localsettings import *
+except ImportError as e:
+    print('No localsettings.py defined: %s' % e)
